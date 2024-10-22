a7467cad126f9c390b85a85280ee2a61e319cc68
Balbaaa <tommyzord@gmail.com>
2024-10-22 13:19:36 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+# Historias para elegir:
+
+# Historia 1
+# La [historia] se sitúa en el pueblo [ficticio] de Hawkins, en el estado de Indiana, Estados Unidos, durante los años ochenta, 
+# [cuando] un niño de doce años llamado Will Byers desaparece [misteriosamente]. 
+# Poco después, Eleven, una niña aparentemente [fugitiva] y con poderes telequinéticos, 
+# se encuentra con Mike, Dustin y Lucas, amigos de Will, y los ayuda en la búsqueda de su amigo desaparecido.
+
+#----------------------------------------------
+
+print("---Actividad - Historia Loca---")
+print()
+
+# 2.- Elegir alguna de las dos historias.
+
+
+# 3.- Seleccionar, dentro la historia elegida, cinco palabras para reemplazar. (Lo puedes hacer agregando [] alrededor de la palabra).
+
+
+# 4.- Crear cinco variables con un input() solicitando al usuario una palabra con la cual se reemplazará la palabra elegida. 
+# (Se recomienda que los nombres de las variables estén relacionados con lo que se pide).
+
+
+# 5.- Mostrar por consola, con un print(), un título para tu historia.
+
+
+# 6.- Dentro de un print(), copiar la historia elegida (Recuerda eliminar los #).
+
+
+# 7.- Reemplazar las palabras seleccionadas concatenando las variables creadas.
+
+nidia = input("¿Es un cuento o una historia?")
+ficcion = input("¿Es real o ficticio?")
+conector = input("¿Cuando comienza?")
+puf = input("¿Como desaparece Will?")
+once = input("¿Eleven esta?")
+
+print("Historia 1")
+print("La" + " " + nidia + " " + "se sitúa en el pueblo" + " " + ficcion + " " + "de Hawkins, en el estado de Indiana, Estados Unidos, durante los años ochenta, ")
+print(conector + " " + "un niño de doce años llamado Will Byers desaparece" + " " + puf )
+print("Poco después, Eleven, una niña aparentemente" + " " + puf + " " + "y con poderes telequinéticos,")
+print("se encuentra con Mike, Dustin y Lucas, amigos de Will, y los ayuda en la búsqueda de su amigo desaparecido.")
